c1e39da1b2f78cc714d9189e240f52a7bedf78ab
Gorev <denis.gorev@accenture.com>
2020-10-10 17:01:16 +0300

    redirecting to the home[age if user has signed up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.components";
 import Header from "./components/header/header.component";
@@ -51,17 +51,23 @@ class App extends React.Component {
 					<Route exact path='/shop'>
 						<ShopPage />
 					</Route>
-					<Route exact path='/signin'>
-						<SignInAndSignUpPage />
-					</Route>
+					<Route
+						exact
+						path='/signin'
+						render={() => (this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />)}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
 	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
